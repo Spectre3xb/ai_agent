54c87d0f053e34b16e247c2d2215914f4e0e683d
Spectre3xb <stefan.g.buchholz@gmail.com>
2025-09-27 13:29:25 +0200

    works as intended... for now

--- system_prompt.py
+++ system_prompt.py
@@ -8,6 +8,8 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write to or overwrite files, pass the content as an argument
 - Execute Python files with optional arguments
 - Give a brief summary of the operations you intend to perform
+- You have to perform function calls until the originally posed question is answered
+- If the query asks for fixing a code, you need to write to the files you identified in previous steps
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
